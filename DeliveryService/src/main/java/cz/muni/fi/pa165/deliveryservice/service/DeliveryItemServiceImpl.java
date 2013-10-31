c759945d7c4a44ce58c5712e1971e750e588fd4c
volnerf <volnerf@localhost.localdomain>
2013-10-31 11:09:00 +0100

    Created DeliveryItemServiceImpl

---
+++ DeliveryService/src/main/java/cz/muni/fi/pa165/deliveryservice/service/DeliveryItemServiceImpl.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.deliveryservice.service;
+
+import cz.muni.fi.pa165.deliveryservice.dto.DeliveryItemDTO;
+
+/**
+ *
+ * @author Filip Volner <volner@mail.muni.cz>
+ */
+public class DeliveryItemServiceImpl implements DeliveryItemService {
+
+    @Override
+    public void createDeliveryItem(DeliveryItemDTO deliveryItem) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteDeliveryItem(DeliveryItemDTO deliveryItem) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public DeliveryItemDTO updateDeliveryItem(DeliveryItemDTO deliveryItem) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public DeliveryItemDTO findDeliveryItem(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
