60be56baadf82f731ccfbed7daa7b43847a1cb44
Jan Vorcak <vorcak@mail.muni.cz>
2013-11-01 19:39:32 +0100

    Added DataPersistenceEx to CourierServiceImpl + Mockito tests for DeliveryService

--- DeliveryService/src/main/java/cz/muni/fi/pa165/deliveryservice/service/CourierServiceImpl.java
+++ DeliveryService/src/main/java/cz/muni/fi/pa165/deliveryservice/service/CourierServiceImpl.java
@@ -3,16 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cz.muni.fi.pa165.deliveryservice.service;
 
 import cz.muni.fi.pa165.deliveryservice.Courier;
 import cz.muni.fi.pa165.deliveryservice.dao.CourierDAO;
 import cz.muni.fi.pa165.deliveryservice.dto.CourierDTO;
+import cz.muni.fi.pa165.deliveryservice.exceptions.DataPersistenceException;
 import java.util.ArrayList;
 import java.util.List;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,46 +27,81 @@ public class CourierServiceImpl implements CourierService {
 
     @Autowired
     private CourierDAO courierDao;
-    
+
     @Autowired
     private Mapper mapper;
-    
+
     @Override
     public CourierDTO createCourier(CourierDTO courierDto) {
-        Courier courier = mapper.map(courierDto, Courier.class);
-        courierDao.createCourier(courier);
-        return mapper.map(courier, CourierDTO.class);
+        if(courierDto == null) {
+            throw new NullPointerException();
+        }
+        
+        try {
+            Courier courier = mapper.map(courierDto, Courier.class);
+            courierDao.createCourier(courier);
+            return mapper.map(courier, CourierDTO.class);
+        } catch (Exception ex) {
+            throw new DataPersistenceException(ex.getMessage());
+        }
     }
 
     @Override
     public void deleteCourier(CourierDTO courierDto) {
-        //TODO throw exception if no id found
-        Courier courier = courierDao.findCourier(courierDto.getId());
-        courierDao.deleteCourier(courier);
+        if(courierDto == null) {
+            throw new NullPointerException();
+        }
+        
+        try {
+            Courier courier = courierDao.findCourier(courierDto.getId());
+            courierDao.deleteCourier(courier);
+        } catch (Exception ex) {
+            throw new DataPersistenceException(ex.getMessage());
+        }
     }
 
     @Override
     public CourierDTO updateCourier(CourierDTO courierDto) {
-        Courier courier = courierDao.findCourier(courierDto.getId());
-        return mapper.map(courierDao.updateCourier(courier), CourierDTO.class);
+        if(courierDto == null) {
+            throw new NullPointerException();
+        }
+        
+        try {
+            Courier courier = courierDao.findCourier(courierDto.getId());
+            return mapper.map(courierDao.updateCourier(courier), CourierDTO.class);
+        } catch (Exception ex) {
+            throw new DataPersistenceException(ex.getMessage());
+        }
     }
 
     @Override
     public List<CourierDTO> getAllCouriers() {
-        List<CourierDTO> resultList = new ArrayList<>();
-        for(Courier courier : courierDao.getAllCouriers()) {
-            resultList.add(mapper.map(courier, CourierDTO.class));
+        try {
+            List<CourierDTO> resultList = new ArrayList<>();
+            for (Courier courier : courierDao.getAllCouriers()) {
+                resultList.add(mapper.map(courier, CourierDTO.class));
+            }
+            return resultList;
+        } catch (Exception ex) {
+            throw new DataPersistenceException(ex.getMessage());
         }
-        return resultList;
     }
 
     @Override
     public CourierDTO findCourier(Long id) {
-        return mapper.map(courierDao.findCourier(id), CourierDTO.class);
+        if(id == null) {
+            throw new NullPointerException();
+        }
+            
+        try {
+            return mapper.map(courierDao.findCourier(id), CourierDTO.class);
+        } catch (Exception ex) {
+            throw new DataPersistenceException(ex.getMessage());
+        }
     }
 
     public void setCourierDao(CourierDAO courierDao) {
         this.courierDao = courierDao;
     }
-     
+
 }
---
+++ DeliveryService/src/test/java/cz/muni/fi/pa165/deliveryservice/service/DeliveryServiceTest.java
@@ -0,0 +1,192 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.muni.fi.pa165.deliveryservice.service;
+
+import cz.muni.fi.pa165.deliveryservice.Courier;
+import cz.muni.fi.pa165.deliveryservice.Customer;
+import cz.muni.fi.pa165.deliveryservice.Delivery;
+import cz.muni.fi.pa165.deliveryservice.DeliveryStatus;
+import cz.muni.fi.pa165.deliveryservice.dao.DeliveryDAO;
+import cz.muni.fi.pa165.deliveryservice.dto.CourierDTO;
+import cz.muni.fi.pa165.deliveryservice.dto.CustomerDTO;
+import cz.muni.fi.pa165.deliveryservice.dto.DeliveryDTO;
+import java.util.ArrayList;
+import java.util.List;
+import org.dozer.DozerBeanMapper;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.dao.DataAccessException;
+import org.springframework.test.util.ReflectionTestUtils;
+
+/**
+ *
+ * @author Jan Vorcak
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DeliveryServiceTest {
+    
+    @Mock
+    private DeliveryDAO deliveryDAO;
+    private DozerBeanMapper mapper;
+    private DeliveryService deliveryService;
+    
+    private Courier courier;
+    private Customer customer;
+    
+    private final static Long findId = 4L;
+    private Delivery delivery, brnoDelivery;
+    private DeliveryDTO deliveryDto, delivery2Dto;
+    private List<Delivery> allDeliveries;
+    
+    public DeliveryServiceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        deliveryService = new DeliveryServiceImpl();
+        mapper = new DozerBeanMapper();
+        ReflectionTestUtils.setField(deliveryService, "deliveryDAO", deliveryDAO);
+        ReflectionTestUtils.setField(deliveryService, "mapper", mapper);
+            
+        courier = new Courier();
+        courier.setEmail("courier@example.com");
+        
+        customer = new Customer();
+        customer.setEmail("customer@example.com");
+        
+        delivery = new Delivery();
+        delivery.setId(findId);
+        delivery.setStatus(DeliveryStatus.PICKED);
+        delivery.setCustomer(customer);
+     
+        brnoDelivery = new Delivery();
+        brnoDelivery.setId(5L);
+        brnoDelivery.setPlaceTo("Brno");
+        brnoDelivery.setStatus(DeliveryStatus.CREATED);
+        brnoDelivery.setCourier(courier);
+        
+        deliveryDto = mapper.map(delivery, DeliveryDTO.class);
+        delivery2Dto = mapper.map(brnoDelivery, DeliveryDTO.class);
+        
+        allDeliveries = new ArrayList<Delivery>() {{
+            add(delivery);
+            add(brnoDelivery);
+        }};
+        
+        when(deliveryDAO.findDelivery(findId)).thenReturn(delivery);
+        when(deliveryDAO.findDelivery(null)).thenThrow(NullPointerException.class);
+        when(deliveryDAO.getAllDeliveries()).thenReturn(allDeliveries);
+        when(deliveryDAO.getDeliveriesByStatus(DeliveryStatus.CREATED)).thenReturn(new ArrayList<Delivery>() {{
+            add(brnoDelivery);
+        }});
+        when(deliveryDAO.getDeliveriesByCourier(courier)).thenReturn(new ArrayList<Delivery>() {{
+            add(brnoDelivery);
+        }});
+        when(deliveryDAO.getDeliveriesByCustomer(customer)).thenReturn(new ArrayList<Delivery>() {{
+            add(delivery);
+        }});
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test(expected = DataAccessException.class)
+    public void testCreateDeliveryWithNull() {
+        deliveryService.createDelivery(null);
+    }
+
+    @Test(expected = DataAccessException.class)
+    public void testFindDeliveryWithNull() {
+        deliveryService.findDelivery(null);
+    }
+    
+    @Test(expected = DataAccessException.class)
+    public void testDeleteDeliveryWithNull() {
+        deliveryService.deleteDelivery(null);
+    }
+    
+    @Test(expected = DataAccessException.class)
+    public void testUpdateDeliveryWithNull() {
+        deliveryService.updateDelivery(null);
+    }
+    
+    @Test
+    public void testFindDelivery() {
+        DeliveryDTO deliveryDtoReturn = deliveryService.findDelivery(findId);
+        Assert.assertEquals(mapper.map(delivery, DeliveryDTO.class), deliveryDtoReturn);
+    }
+    
+    @Test
+    public void testCreateDelivery() {
+        deliveryService.createDelivery(deliveryDto);
+        verify(deliveryDAO).createDelivery(delivery);
+    }
+    
+    @Test
+    public void testDeleteDelivery() {
+        deliveryService.deleteDelivery(deliveryDto);
+        verify(deliveryDAO).deleteDelivery(delivery);
+    }
+    
+    @Ignore
+    @Test
+    public void testUpdateDelivery() {
+        deliveryService.updateDelivery(deliveryDto);
+        verify(deliveryDAO).updateDelivery(delivery);
+    }
+    
+    @Test
+    public void testGetAllDeliveries() {
+        List<DeliveryDTO> deliveriesDto = deliveryService.getAllDeliveries();
+        verify(deliveryDAO).getAllDeliveries();
+        
+        Assert.assertEquals(new ArrayList<DeliveryDTO>() {{
+            add(mapper.map(delivery, DeliveryDTO.class));
+            add(mapper.map(brnoDelivery, DeliveryDTO.class));
+        }}, deliveriesDto);
+    }
+    
+    @Test
+    public void testGetDeliveriesByStatus() {
+        Assert.assertEquals(new ArrayList<DeliveryDTO>() {{
+            add(mapper.map(brnoDelivery, DeliveryDTO.class));
+        }}, deliveryService.getDeliveriesByStatus(DeliveryStatus.CREATED));
+    }
+    
+    @Test
+    public void testGetDeliveriesByCourier() {
+        Assert.assertEquals(new ArrayList<DeliveryDTO>() {{
+            add(mapper.map(brnoDelivery, DeliveryDTO.class));
+        }}, deliveryService.getDeliveriesByCourier(mapper.map(courier, CourierDTO.class)));
+    }
+    
+    @Test
+    public void testGetDeliveriesByCustomer() {
+        Assert.assertEquals(new ArrayList<DeliveryDTO>() {{
+            add(mapper.map(delivery, DeliveryDTO.class));
+        }}, deliveryService.getDeliveriesByCustomer(mapper.map(courier, CustomerDTO.class)));
+    }   
+}
